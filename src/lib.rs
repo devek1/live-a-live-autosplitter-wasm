26787dbe527d963247beb2c5efb5f497ce4ba85a
William Volin <me@williamvolin.com>
2023-06-01 13:18:55 -0700

    Update lib.rs
    loop internals on restart

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ asr::async_main!(stable);
 
 async fn main() {
     // TODO: Set up some general state and settings.
-    let process = Process::wait_attach("LIVEALIVE-Win64-Shipping.exe").await;
+
 
     // let mut start = Watcher::<u8>::new(vec![0x4A2DA88, 0x20, 0x1B8, 0x110, 0x28]); // CurrentGameChapterID
     // let mut loading = Watcher::<u8>::new(vec![0x5092A98, 0x8, 0x10, 0x1f8, 0x50, 0x30, 0x3FA]); // CurrentGameChapterID
@@ -21,109 +21,112 @@ async fn main() {
     let mut loading_watcher = Watcher::<u8>::new(); // CurrentGameChapterID
     let mut splits = HashSet::<String>::new();
     let settings = Settings::register();
-    let (main_module_base, _main_module_size) = process
-        .wait_module_range("LIVEALIVE-Win64-Shipping.exe")
-        .await;
+    loop {
+        let process = Process::wait_attach("LIVEALIVE-Win64-Shipping.exe").await;
+        let (main_module_base, _main_module_size) = process
+            .wait_module_range("LIVEALIVE-Win64-Shipping.exe")
+            .await;
 
-    process
-        .until_closes(async {
-            // TODO: Load some initial information from the process.
-            loop {
-                let start = start_watcher
-                    .update(
-                        process
-                            .read_pointer_path64(
-                                main_module_base,
-                                &vec![0x4A2DA88, 0x20, 0x1B8, 0x110, 0x28],
-                            )
-                            .ok(),
-                    )
-                    .unwrap();
+        process
+            .until_closes(async {
+                // TODO: Load some initial information from the process.
+                loop {
+                    let start = start_watcher
+                        .update(
+                            process
+                                .read_pointer_path64(
+                                    main_module_base,
+                                    &vec![0x4A2DA88, 0x20, 0x1B8, 0x110, 0x28],
+                                )
+                                .ok(),
+                        )
+                        .unwrap();
 
-                let new_start_value = match process.read_pointer_path64(
-                    main_module_base,
-                    &vec![0x508ACE0, 0x10, 0xB0, 0xE0, 0x348],
-                ) {
-                    Ok(val) => Some(val),
-                    Err(_e) => Some(0),
-                };
+                    let new_start_value = match process.read_pointer_path64(
+                        main_module_base,
+                        &vec![0x508ACE0, 0x10, 0xB0, 0xE0, 0x348],
+                    ) {
+                        Ok(val) => Some(val),
+                        Err(_e) => Some(0),
+                    };
 
-                let new_start = new_game_start_watcher.update(new_start_value).unwrap();
+                    let new_start = new_game_start_watcher.update(new_start_value).unwrap();
 
-                let loading = loading_watcher
-                    .update(
-                        process
-                            .read_pointer_path64(
-                                main_module_base,
-                                &vec![0x5092A98, 0x8, 0x10, 0x1f8, 0x50, 0x30, 0x3FA],
-                            )
-                            .ok(),
-                    )
-                    .unwrap();
+                    let loading = loading_watcher
+                        .update(
+                            process
+                                .read_pointer_path64(
+                                    main_module_base,
+                                    &vec![0x5092A98, 0x8, 0x10, 0x1f8, 0x50, 0x30, 0x3FA],
+                                )
+                                .ok(),
+                        )
+                        .unwrap();
 
-                match timer::state() {
-                    TimerState::NotRunning => {
-                        if settings.start && start.old == 9 && start.current != 9 {
-                            // asr::print_message("Clearing Splits and Starting");
-                            splits = HashSet::<String>::new();
-                            timer::start();
-                        }
-
-                        if settings.new_start && new_start.old == 0 && new_start.current == 158 {
-                            // asr::print_message("Clearing Splits and Starting");
-                            splits = HashSet::<String>::new();
-                            timer::start();
-                        }
-                    }
-                    TimerState::Running => {
-                        // CHAPTER SPLITS
-
-                        if settings.start_prehistory && start.old == 9 && start.current == 1 {
-                            split(&mut splits, "start_prehistory")
-                        }
-                        if settings.start_distant_future && start.old == 9 && start.current == 2 {
-                            split(&mut splits, "start_distant_future")
-                        }
-                        if settings.start_imperial_china && start.old == 9 && start.current == 3 {
-                            split(&mut splits, "start_imperial_china")
-                        }
-                        if settings.start_wild_west && start.old == 9 && start.current == 4 {
-                            split(&mut splits, "start_wild_west")
-                        }
-                        if settings.start_present_day && start.old == 9 && start.current == 5 {
-                            split(&mut splits, "start_present_day")
-                        }
-                        if settings.start_near_future && start.old == 9 && start.current == 6 {
-                            split(&mut splits, "start_near_future")
-                        }
-                        if settings.start_twilight_of_edo_japan && start.old == 9 && start.current == 7 {
-                            split(&mut splits, "start_twilight_of_edo_japan")
-                        }
-                        if settings.start_middle_ages && start.old == 9 && start.current == 0 {
-                            split(&mut splits, "start_middle_ages")
-                        }
-                        if settings.start_dominion_of_hate && start.old == 9 && start.current == 7 {
-                            split(&mut splits, "start_dominion_of_hate")
-                        }
-
-                        if settings.load_removal {
-                            // load/save removal
-                            if loading.old == 0 && loading.current == 1 {
-                                timer::resume_game_time()
+                    match timer::state() {
+                        TimerState::NotRunning => {
+                            if settings.start && start.old == 9 && start.current != 9 {
+                                // asr::print_message("Clearing Splits and Starting");
+                                splits = HashSet::<String>::new();
+                                timer::start();
                             }
 
-                            if loading.old == 1 && loading.current == 0 {
-                                timer::pause_game_time()
+                            if settings.new_start && new_start.old == 0 && new_start.current == 158 {
+                                // asr::print_message("Clearing Splits and Starting");
+                                splits = HashSet::<String>::new();
+                                timer::start();
                             }
                         }
+                        TimerState::Running => {
+                            // CHAPTER SPLITS
+
+                            if settings.start_prehistory && start.old == 9 && start.current == 1 {
+                                split(&mut splits, "start_prehistory")
+                            }
+                            if settings.start_distant_future && start.old == 9 && start.current == 2 {
+                                split(&mut splits, "start_distant_future")
+                            }
+                            if settings.start_imperial_china && start.old == 9 && start.current == 3 {
+                                split(&mut splits, "start_imperial_china")
+                            }
+                            if settings.start_wild_west && start.old == 9 && start.current == 4 {
+                                split(&mut splits, "start_wild_west")
+                            }
+                            if settings.start_present_day && start.old == 9 && start.current == 5 {
+                                split(&mut splits, "start_present_day")
+                            }
+                            if settings.start_near_future && start.old == 9 && start.current == 6 {
+                                split(&mut splits, "start_near_future")
+                            }
+                            if settings.start_twilight_of_edo_japan && start.old == 9 && start.current == 7 {
+                                split(&mut splits, "start_twilight_of_edo_japan")
+                            }
+                            if settings.start_middle_ages && start.old == 9 && start.current == 0 {
+                                split(&mut splits, "start_middle_ages")
+                            }
+                            if settings.start_dominion_of_hate && start.old == 9 && start.current == 7 {
+                                split(&mut splits, "start_dominion_of_hate")
+                            }
+
+                            if settings.load_removal {
+                                // load/save removal
+                                if loading.old == 0 && loading.current == 1 {
+                                    timer::resume_game_time()
+                                }
+
+                                if loading.old == 1 && loading.current == 0 {
+                                    timer::pause_game_time()
+                                }
+                            }
+                        }
+                        _ => {}
                     }
-                    _ => {}
+                    // TODO: Do something on every tick.
+                    next_tick().await;
                 }
-                // TODO: Do something on every tick.
-                next_tick().await;
-            }
-        })
-        .await;
+            })
+            .await;
+    }
 }
 
 pub fn split(splits: &mut HashSet<String>, key: &str) {
